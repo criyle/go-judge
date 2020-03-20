91c8c321c4894b45d7ee8eb841af08c062fc4ca8
criyle <gaoyang4425@gmail.com>
2020-03-20 19:05:10 -0400

    add ability to not unshare net

--- cmd/executorserver/main.go
+++ cmd/executorserver/main.go
@@ -14,6 +14,7 @@ import (
 	"github.com/criyle/go-judge/pkg/pool"
 	"github.com/criyle/go-sandbox/container"
 	"github.com/criyle/go-sandbox/pkg/cgroup"
+	"github.com/criyle/go-sandbox/pkg/forkexec"
 	"github.com/criyle/go-sandbox/pkg/mount"
 	"github.com/gin-gonic/gin"
 )
@@ -24,6 +25,7 @@ var (
 	tmpFsParam = flag.String("tmpfs", "size=8m,nr_inodes=4k", "tmpfs mount data")
 	dir        = flag.String("dir", "", "specifies direcotry to store file upload / download (in memory by default)")
 	silent     = flag.Bool("silent", false, "do not print logs")
+	netShare   = flag.Bool("net", false, "do not unshare net namespace with host")
 
 	envPool    envexec.EnvironmentPool
 	cgroupPool envexec.CgroupPool
@@ -85,13 +87,19 @@ func main() {
 	}
 	printLog("Created default container mount at:", mb)
 
+	unshareFlags := uintptr(forkexec.UnshareFlags)
+	if *netShare {
+		unshareFlags ^= syscall.CLONE_NEWNET
+	}
+
 	b := &container.Builder{
 		Root:          root,
 		Mounts:        m,
 		CredGenerator: newCredGen(),
 		Stderr:        true,
+		CloneFlags:    unshareFlags,
 	}
-	cgb, err := cgroup.NewBuilder("go-judger").WithCPUAcct().WithMemory().WithPids().FilterByEnv()
+	cgb, err := cgroup.NewBuilder("executorserver").WithCPUAcct().WithMemory().WithPids().FilterByEnv()
 	if err != nil {
 		panic(err)
 	}
--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/criyle/go-judge
 go 1.14
 
 require (
-	github.com/criyle/go-sandbox v0.2.1
+	github.com/criyle/go-sandbox v0.3.0
 	github.com/gin-gonic/gin v1.5.0
 	github.com/go-playground/universal-translator v0.17.0 // indirect
 	github.com/golang/protobuf v1.3.5 // indirect
@@ -15,7 +15,7 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.1 // indirect
 	github.com/ugorji/go/codec v1.1.7
-	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e
+	golang.org/x/sync v0.0.0-20200317015054-43a5402ce75a
 	golang.org/x/sys v0.0.0-20200316230553-a7d97aace0b0 // indirect
 	gopkg.in/go-playground/validator.v9 v9.31.0 // indirect
 	gopkg.in/yaml.v2 v2.2.8 // indirect

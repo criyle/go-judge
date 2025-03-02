897d0453022e7f39189994753b8273e03407a078
criyle <gaoyang4425@gmail.com>
2025-03-02 20:32:49 +0000

    pb: split messages according to best practice

--- pb/gen.go
+++ pb/gen.go
@@ -1,3 +1,3 @@
 package pb
 
-//go:generate protoc --proto_path=./ --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative judge.proto
+//go:generate protoc --proto_path=./ --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative judge.proto request.proto response.proto stream_request.proto stream_response.proto file.proto
